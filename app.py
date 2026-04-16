511bfb44f1f42d1a1b835e56aa55426511235af2
suraj142001 <surajgavadecloudblitz@gmail.com>
2026-04-16 16:50:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ import pandas as pd
 import urllib.parse
 import os
 
+import os
+
+col1, col2 = st.columns([1,5])
+
+with col1:
+    if os.path.exists("logo.png"):
+        st.image("logo.png", width=80)
+
+with col2:
+    st.markdown("<h2>राजहंस पुस्तक पेठ , पुणे ०३८</h2>", unsafe_allow_html=True)
+    st.markdown("🎉 ऑफर कालावधी : १६ एप्रिल ते १९ एप्रिल २०२६ पर्यन्त")
+    
 # =========================
 # SHOP INFO
 # =========================
